5ff939bf11383e52685766d3b856646d8dfa142f
Chandra Prakash Tiwari <cptiwari20@gmail.com>
2023-01-04 21:25:53 +0530

    added basic struct

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/cptiwari20/go-blockchain
 
 go 1.19
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -2,16 +2,52 @@ package main
  
 import (
 	"log"
+	"encoding/json"
 	"net/http"
 	"github.com/gorilla/mux"
 )
+
+// Structs
+type Block struct {
+
+}
+
+type BookCheckout struct {
+
+}
+
+type Book struct {
+	ID			string		`json:"id"`
+	Author 		string		`json:"author"`
+	PublishDate	string		`json:"publish_date"`
+	CreatedAt	string		`json:"created_at"`
+	ISBT		string		`json:"isbt"`
+
+}
+
+type Blockchain {
+	blocks []*Block
+}
+
+func newBook (w http.ResponseWriter, r *http.Request) {
+	var book Book
+
+	err := json.NewDecoder(r.Body).Decode(&book); err !=nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("Could not create :%v", err)
+		w.Write([]byte("could not create a new book"))
+		return
+	}
+
+	h := md5.New()
+}
 
 func main(){
 	r := mux.NewRouter()
 	r.HandleFunc("/", getBlockchain).Methods("GET")
 	r.HandleFunc("/", writeBlock).Methods("POST")
-	r.HandleFunc("/new", newBlock).Methods("POST")
+	r.HandleFunc("/new", newBook).Methods("POST")
 
 	log.Println("Listening on the port 3000")
-	log.Fatal(http.listenAndServe(":3000", r))
+	log.Fatal(http.ListenAndServe(":3000", r))
 }
